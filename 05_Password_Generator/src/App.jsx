f58219a36d92cefce950cc42aa7916e9110e568e
Rudr7_7 <ankitgautam50366@gmail.com>
2025-10-07 12:36:20 +0530

    Working on range feature

--- 05_Password_Generator/src/App.jsx
+++ 05_Password_Generator/src/App.jsx
@@ -1,34 +1,58 @@
-import { useState , useEffect} from 'react'
+import { useState , useCallback} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(8)
+  const [numAllowed , setnumAllowed] = useState(false)
+  const [ charAllowed , setcharAllowed] = useState(false)
+  const [ password , setPassword] = useState('')
+  
+  const passwordGenerator = useCallback( ()=> {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()_+?><:{}[]"
+    for (let i = 1 ; i <= array.lengtht ; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+      
+    }
+    setPassword(pass)
 
-  useEffect(() => {
-    console.log("Count changed:", count);
-  }, [count]);
+  }, [length , numAllowed , charAllowed , setPassword])
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <h2 className="text-5xl font-bold mb-6">Count: {count}</h2>
 
-      <div className="flex gap-4">
-        <button
-          onClick={() => setCount(count + 1)}
-          className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-xl shadow-md transition transform hover:scale-105 active:scale-95"
-        >
-          +
-        </button>
+  return(
 
-        <button
-          onClick={() => setCount(count - 1)}
-          className="bg-red-500 hover:bg-red-600 text-white px-6 py-2 rounded-xl shadow-md transition transform hover:scale-105 active:scale-95"
-        >
-          -
-        </button>
+  <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800 text-center'>
+     <h1 className='text-white text-center my-3'>Password Generator</h1>
+    <div className='flex shadow rounded-lg overflow-hidden mb-4 bg-white text-gray-800'>
+      <input type="text"
+       value={password}
+       className='outline-none w-full py-1 px-3'
+       placeholder='Password'
+       readOnly 
+      />
+      <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+      >Copy</button>
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range" 
+        min = {6}
+        max = {100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => setLength(e.target.value)}
+          />
+           <label>Label : {length}</label>
+
       </div>
     </div>
-  );
+  </div>
+
+  )
 }
 
 export default App
